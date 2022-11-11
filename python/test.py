e026d36ce328d32b9b043a2af90ab7f27e8f0d02
akalman Nakarmi <scodeAllDay@gmail.com>
2022-11-11 18:18:16 +0545

    Sum
    added sum function for adding numbers

--- python/test.py
+++ python/test.py
@@ -2,14 +2,18 @@ import time
 
 
 def run():
-    print("Hello World")
-
-
-print("Hello World")
-print("Really?")
+    a = int(input("Enter a Number: ")) 
+    b = int(input("Enter a Number: ")) 
+    print(f"Sum: {sum([a,b])}")
+    
+def sum(a):
+    result = 0
+    for v in a:
+        result += v
+    return result
 
 
 if __name__ == "__main__":
     startTime = time.time()
     run()
-    print(f"Time Taken: {time.time()-startTime}")
+    print(f"Time Taken: {int(time.time()-startTime)} ms")
